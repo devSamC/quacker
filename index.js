9989c3c4ec8a7b6fa5dc378110e7bf39de332c11
brodie-m <brodiedmcguire@gmail.com>
2021-09-11 09:30:47 +0100

    more backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const importData = require('./data.json');
-
+const cors = require('cors')
 
 let port = process.env.PORT || 3000;
 app.get('/',(req,res) => {
@@ -10,6 +10,14 @@ app.get('/',(req,res) => {
 
 app.get('/players',(req,res) => {
     res.send(importData)
+})
+
+app.post('/players', (req,res) => {
+    const data = req.body;
+    const newUser = User.create(data);
+    res.send({
+        message: `${newUser.name} added to collection`
+    })
 })
 
 app.listen(port, () => {
