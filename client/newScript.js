dcb0d17a44c3beae3e17a83f00f0845d4d9321bc
brodie-m <brodiedmcguire@gmail.com>
2021-09-13 17:03:58 +0100

    cors is back in

--- client/newScript.js
+++ client/newScript.js
@@ -3,7 +3,9 @@ submitButton.addEventListener('click', e => addQuack(e))
 // const axios = require('axios')
 async function generateCard() {
     const posts = await fetch('https://quackerapi-nodejs.herokuapp.com/players')
+    console.log(posts)
     const postsData = await posts.json()
+    console.log(postsData)
     const postBox = document.getElementById('quack-test-holder');
     const newPost = document.createElement('div');
     const newPostText = document.createElement('p')
@@ -13,9 +15,9 @@ async function generateCard() {
 }
 
 
-async function addQuack(e) {
+function addQuack(e) {
     e.preventDefault();
-    
+    console.log('clicked')
     //send post data to server and then retrieve
     //first just console log the data that we get
     // const quackBox = document.getElementById('quack-input');
@@ -24,6 +26,3 @@ async function addQuack(e) {
     generateCard();
 }
 
-function generateCard() {
-
-}
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const importData = require('./data.js');
 const User = require(`./models/users`)
-// const cors = require('cors')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 let port = process.env.PORT || 3000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json())
 app.get('/',(req,res) => {
