fa0eeced2e861c3aaeb6d5c9e2445c49e4706790
brodie-m <brodiedmcguire@gmail.com>
2021-09-13 19:40:24 +0100

    fixing routes

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const importData = require('./data.js');
+const User = require(`./models/users`)
+const Post = require('./models/posts');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+let port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json())
+app.get('/',(req,res) => {
+    res.send({
+        message: `Hello World`
+    })
+})
+
+app.get('/players',(req,res) => {
+    res.send(importData)
+})
+
+app.post('/players', (req,res) => {
+    // console.log(req)
+    const data = req.body
+    console.log(data)
+    const newPost = Post.create(data)
+    res.send({
+        message: `${newPost.text} added to collection`
+    })
+})
+
+app.listen(port, () => {
+    console.log(`twitter clone listening on port ${port}`)
+})
+
+// module.exports = index
---
+++ server/models/users.js
@@ -0,0 +1,23 @@
+const usersData = require('../data.js')
+
+class User {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name
+        this.picture = data.picture;
+        this.posts = data.posts;
+    }
+
+    static get all() {
+        const users = usersData.map((user) => new User(user));
+        return users;
+    }
+
+    static create(user) {
+        const newUser = new User({...user})
+        usersData.push(newUser);
+        return newUser;
+    }
+}
+
+module.exports = User;
