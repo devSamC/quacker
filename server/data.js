992d0668a78b9d612dff7babd00bb379bcaef96b
brodie-m <brodiedmcguire@gmail.com>
2021-09-14 10:14:41 +0100

    updated data.js to include proper dates

--- server/data.js
+++ server/data.js
@@ -4,21 +4,21 @@ const postsData = [{
     "picture": "",
     "reactions": "",
     "comments": "",
-    "date": "Tue, 14 Sep 2021 07:46:51 GMT"
+    "date": "Sun, 12 Sep 2021 09:45:55 GMT"
 }, {
     "id": 2,
     "text": "my second quack. wow i love coding",
     "picture": "",
     "reactions": "",
     "comments": "",
-    "date": "Sun, 12 Sep 2021 09:45:55 GMT"
+    "date": "Mon, 13 Sep 2021 09:45:55 GMT"
 }, {
     "id": 3,
     "text": "my third quack. wow i love coding",
     "picture": "",
     "reactions": "",
     "comments": "",
-    "date": ""
+    "date": "Tue, 14 Sep 2021 09:45:55 GMT"
 }]
 
 module.exports = postsData;
