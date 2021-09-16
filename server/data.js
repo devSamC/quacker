eb1b6fca49341cbb48b6bd82a827cf2df0babe5b
brodie-m <brodiedmcguire@gmail.com>
2021-09-16 09:16:41 +0100

    forgot to add data.js to last commit

--- server/data.js
+++ server/data.js
@@ -25,12 +25,14 @@ const postsData = [{
     "reactions": [{id: 1, count: 0},{id: 2, count: 0},{id: 3, count: 0}],
     "comments": [{
             id: 1,
+            author: delayed-artistic-Guppy,
             text: "me too!",
             reactions: [{id: 1, count: 1},{id: 2, count: 2},{id: 3, count: 3}],
             date: "Tue, 14 Sep 2021 11:45:55 GMT"
         },
         {
             id: 2,
+            author: wild-wild-Rasool,
             text: "me as well! wow!",
             reactions: [{id: 1, count: 0},{id: 2, count: 0},{id: 3, count: 0}],
             date: "Tue, 14 Sep 2021 12:45:55 GMT"
